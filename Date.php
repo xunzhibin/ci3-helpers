5c02d6f438236e122a1461e514cdffb081fedf37
xunzhibin <xunzhibin@jnexpert.com>
2024-06-19 18:10:58 +0800

    日期 辅助函数

---
+++ Date.php
@@ -0,0 +1,17 @@
+<?php
+
+// 命名空间
+namespace Xzb\Ci3\Helpers;
+
+// 第三方 日期时间类 https://carbon.nesbot.com/docs/
+use Carbon\Carbon;
+
+// "nesbot/carbon": "2.*",
+
+/**
+ * 日期
+ */
+class Date extends Carbon
+{
+
+}
