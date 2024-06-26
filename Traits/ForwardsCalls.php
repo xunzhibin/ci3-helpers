cff94a3ae8539e9ff6212598ae77b15e4904a1f0
xunzhibin <xunzhibin@jnexpert.com>
2024-06-26 14:38:08 +0800

    类trait 调用转发

---
+++ Traits/ForwardsCalls.php
@@ -0,0 +1,48 @@
+<?php
+
+// 命名空间
+namespace Xzb\Ci3\Helpers\Traits;
+
+// PHP 异常类
+use BadMethodCallException;
+use Error;
+
+/**
+ * 转接呼叫
+ */
+trait ForwardsCalls
+{
+    /**
+     * 将 方法调用 转发到 给定对象
+     * 
+     * @param object $object
+     * @param string $method
+     * @param array $parameters
+     * @return mixed
+     */
+    protected function forwardCallTo($object, $method, $parameters)
+    {
+        try {
+            return $object->{$method}(...$parameters);
+        }
+        catch (Error|BadMethodCallException $e) {
+            $pattern = '~^Call to undefined method (?P<class>[^:]+)::(?P<method>[^\(]+)\(\)$~';
+
+            if (! preg_match($pattern, $e->getMessage(), $matches)) {
+                throw $e;
+            }
+
+            if ($matches['class'] != get_class($object) || $matches['method'] != $method) {
+                throw $e;
+            }
+
+			throw new BadMethodCallException(sprintf(
+				'Call to undefined method %s::%s()', static::class, $method
+			));
+        }
+        catch (Throwable $e) {
+            throw $e;
+        }
+    }
+
+}
